689f1ce1d1d7ebfa9eccf7379860bdd773d365dc
Aimable <aimatwiri@gmail.com>
2019-11-25 11:16:15 +0200

    passenger views and create

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,22 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end
+  def new
+    @passenger = Passenger.new
+  end
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passengers_path, notice: 'new passenger registered'
+    else
+      render:new
+    end
+  end
+
+  private
+
+  def passenger_params
+    params.require(:passenger).permit(:first_name, :last_name, :email, :phone, :id_number, :destination, :ref_first_name, :ref_last_name, :ref_phone_number, :entry_on, :return_on)
+  end
 end
