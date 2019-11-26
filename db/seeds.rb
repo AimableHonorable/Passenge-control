3ec21af12ee17f1ff56f6b49d8644930a2afd724
Aimable <aimatwiri@gmail.com>
2019-11-27 01:53:34 +0200

    correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create(first_name: 'Aimable',
-           last_name: 'Twiringiyimana',
-           email: 'aimable@gmail.com',
-           password: 'password',
-           admin: true)
 Passenger.create(first_name: 'Prince',
                  last_name: 'gashema',
                  email: 'prince1@gmail.com',
